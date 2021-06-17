309509a3c9de8a83f614c15d122d34c3c099e802
Laurajcb <3398@holbertonshcool.com>
2021-06-17 16:10:43 -0700

    fix number 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main - display with proper grammar, but the outcome is a piece of art
+ *return :0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
