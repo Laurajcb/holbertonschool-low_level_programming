4cfb5119da1164a2985419c51c071df9f52b8670
Laurajcb <3398@holbertonshcool.com>
2021-06-18 16:18:42 -0700

    adding more tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Void always return: 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+for (n = 97; n <= 122; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
