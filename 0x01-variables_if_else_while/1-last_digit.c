d16e7de24e3a7d8dd9f9c12cf4ed6950ef25aaaf
Laurajcb <3398@holbertonshcool.com>
2021-06-18 15:34:33 -0700

    Fix string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ printf("Last digit of %d is %d and is 0\n", n, d);
 }
 else if (d < 6 && d != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, d);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 }
 return (0);
 }
