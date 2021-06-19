5b969e15c854678059b47fae933af40b3be538d9
Laurajcb <3398@holbertonshcool.com>
2021-06-18 19:04:18 -0700

    Adding number 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints that prints all possible combinations of single-digit numbers.
+ * Void always return: 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+putchar(44);
+putchar(32);
+}
+
+putchar('\n');
+
+return (0);
+}
