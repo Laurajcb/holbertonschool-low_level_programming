2583e428af2f82927eaa7b6b256d52b732f1c49a
Laurajcb <3398@holbertonshcool.com>
2021-06-18 17:25:54 -0700

    Adding number 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ * Void always return: 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+}
