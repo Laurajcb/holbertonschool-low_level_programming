cab561e724d174ec3d7d75d62781b3c801bbc6e2
Laurajcb <3398@holbertonshcool.com>
2021-06-18 17:43:19 -0700

    Adding number 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ * Void always return: 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+for (n = 122; n >= 97; n--)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+}
