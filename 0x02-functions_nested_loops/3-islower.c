65b2f9c20e2fc8f32835fae96f42102d58111334
Laurajcb <3398@holbertonshcool.com>
2021-06-22 08:21:44 -0700

    fixing error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,20 +12,16 @@ int _islower(int c)
 {
 	int r;
 
-if (c >= 97 && c <= 122)
-{
-	r = 1;
-}
-
 if (c >= 65 && c <= 90)
 {
 	r = 0;
 }
 
-else
+if (c >= 97 && c <= 122)
 {
-	r = 0;
+	r = 1;
 }
+
 
 return (r);
 }
