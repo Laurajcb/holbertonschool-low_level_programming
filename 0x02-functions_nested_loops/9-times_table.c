da2c41ef70935c8164367537e72550860dface73
Laurajcb <3398@holbertonshcool.com>
2021-06-22 12:16:10 -0700

    adding 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *times_table-(void)prints the 9 times table, starting with 0.
+ * void
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int s;
+	int n;
+	int product;
+for (s = 0; s < 9; s++)
+{
+	_putchar('0');
+	for (n = 0; n < 9; n++)
+{
+	_putchar(',');
+	_putchar(' ');
+
+	product = s * n;
+
+if (product <= 9)
+
+	_putchar(32);
+	else
+
+	_putchar((product / 10) + '0');		
+	_putchar((product % 10) + '0');
+}
+_putchar('\n');
+
+}
+
+}
