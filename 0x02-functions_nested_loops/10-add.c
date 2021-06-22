507f520bbfd147f065dd298c2dae90c53f857a49
Laurajcb <3398@holbertonshcool.com>
2021-06-22 13:11:05 -0700

    Adding ten to my repo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *add-function - adds two integers and returns the result.
+ *@a:number that is goint to be add to another number
+ *@b:the other number.
+ * Return: a plus b the addition of 2 numbers.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
