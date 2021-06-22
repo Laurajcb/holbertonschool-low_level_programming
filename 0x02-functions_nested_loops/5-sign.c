13382588d4105c3b2e93065e782e6229830b1726
Laurajcb <3398@holbertonshcool.com>
2021-06-22 09:23:35 -0700

    fixed point five

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,14 +17,14 @@ if (n > 0)
 	return (1);
 }
 
-else if (n == 0)
+else if (n < 0)
 {
-	_putchar(48);
-	return (0);
+	_putchar(45);
+	return (-1);
 }
 else
-{	putchar(45);
-	return (-1);
+{	putchar(48);
+	return (0);
 }
 
 
