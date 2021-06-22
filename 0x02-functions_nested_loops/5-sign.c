ef61222c6eae1f9ae119919c91599644e047c0f3
Laurajcb <3398@holbertonshcool.com>
2021-06-22 09:13:04 -0700

    fixed point five

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,29 +3,29 @@
 #include <ctype.h>
 
 /**
- * _isalpha - Prints the sing of a number.
+ * print_sign-Prints the sing of a number.
  * @n :the character with ascii code  .
  * Return: 1 and prints + if n is greater,otherwise print 0 or less
  */
 
 int print_sign(int n)
 {
-	int r;
 
 if (n > 0)
 {
-	_putchar('+');
-	r = 1;
+	_putchar(43);
+	return (1);
 }
 
 else if (n == 0)
 {
-	_putchar('0');
-	r = 0;
+	_putchar(48);
+	return (0);
 }
 else
-{	putchar('-');
-	r = 1;
+{	putchar(45);
+	return (-1);
 }
-return (r);
+
+
 }
