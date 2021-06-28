b7ab3b6623c09c3e37051639b086cff7b0060f3f
Laurajcb <3398@holbertonshcool.com>
2021-06-28 16:45:59 -0700

    added number 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_rev- prints a string, in reverse \n .
+ *@s: that is the char
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
