9e9149e4883469532ff4daca0d566030e5e12633
Laurajcb <3398@holbertonshcool.com>
2021-06-29 10:41:48 -0700

    ADDED point 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * puts2- prints every other character of a string \n .
+ *@str: that is the char
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+		_putchar (str[i]);
+
+	}
+	_putchar('\n');
+}
