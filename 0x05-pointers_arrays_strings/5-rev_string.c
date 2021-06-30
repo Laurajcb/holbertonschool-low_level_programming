36c878e296604be328dc0f78be17c2a85dc6da87
Laurajcb <3398@holbertonshcool.com>
2021-06-29 19:10:28 -0700

    fix 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * rev_string- prints a string, in reverse \n .
+ *@s: that is the char
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char tmp = 0;
+
+	while (s[i])
+	{
+		i++;
+		len++;
+	}
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+
+
+}
