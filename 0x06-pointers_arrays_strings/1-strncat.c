80aa88f9fa51e51603e6446f1609a937c52b5552
Laurajcb <3398@holbertonshcool.com>
2021-07-01 19:23:58 -0700

    added point 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strcat- funtion that cat 2 strings 
+ *@dest: where we appends src
+ *@src: the source string to cp
+ *@n:int
+ * Return: cahr
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		
+		
+	}
+	for (j = 0; j < n && src[j] != '\0';j++)
+
+	{
+		dest[i+j] = src[j];
+	
+	}
+
+	return (dest);
+}
