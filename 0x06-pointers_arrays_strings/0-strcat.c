2b2664370a82fdfea7abb857db6b99bdc6a5c2bf
Laurajcb <3398@holbertonshcool.com>
2021-07-01 19:30:51 -0700

    fixed 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -22,13 +22,13 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	for (i = leng_dest; src[j] != '\0'; i++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		j++;
+		dest[i + j] = src[j];
+	
 	}
 
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
