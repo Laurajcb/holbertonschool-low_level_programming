caebccd64cd27a71b311c2e7375f6890292a4594
Laurajcb <3398@holbertonshcool.com>
2021-07-01 20:20:23 -0700

    added number 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strcat- funtion that cp a strings
+ *@dest: where copy
+ *@src: the source string
+ *@n: charaters form src
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+
+	{
+		dest[i] = src[i];
+
+	}
+
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+
+	return (dest);
+}
