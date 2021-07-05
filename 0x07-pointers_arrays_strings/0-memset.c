b73adfada5261302d7f36aaa2df2bc757361ae21
Laurajcb <3398@holbertonshcool.com>
2021-07-05 08:48:28 -0700

    Added new readme jolberton.h and point 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+*_memset- funtion that fills memmory whit a constant byte
+*@s: where the pointer is pointing 
+*@b: is the constant byte
+*@n:bytes of the memory area to change
+* Return: pointer 
+**/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i]= b;
+		
+	}
+	return(s);
+
+
+}	
