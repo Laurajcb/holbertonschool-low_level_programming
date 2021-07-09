2cb686462f0b93fb1b5238921d1f8d3dec8f0de4
Laurajcb <3398@holbertonshcool.com>
2021-07-09 14:40:21 -0700

    added number 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * factorial- returns the factorial
+ * @n:number
+ * Return: 0
+ */
+
+int factorial(int n)
+{
+
+	if (n == 0)
+	{
+	return (1);
+	}
+
+	if (n < 0)
+	{
+	return (-1);
+	}
+	return (n * factorial(n - 1));
+}
