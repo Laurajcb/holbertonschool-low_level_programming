7c235a1ecb48588f70e2bd8343cdd606c6a76e04
Laurajcb <callejaslaura96@gmail.com>
2021-07-20 01:42:24 +0000

    fixed error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,7 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (j = 0; owner[j] != '\0'; j++)
 	{
 	}
-	cp1 = malloc((sizeof(char) * i )+ 1);
+	cp1 = malloc((sizeof(char) * i) + 1);
 	if (cp1 == NULL)
 	{
 		free(doggi);
