8048d02fffe00cab9c1724877a07fd63f808e18e
Laurajcb <callejaslaura96@gmail.com>
2021-07-21 16:48:17 +0000

    fix header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
-#ifndefine FUNCTION_TO_POINTERS
+#ifndef FUNCTION_TO_POINTERS
 #define FUNCTION_TO_POINTERS
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
