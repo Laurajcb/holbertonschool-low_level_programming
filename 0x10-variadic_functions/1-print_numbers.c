7d58a53eb43e131d4c49a790436d13104de0678d
Laurajcb <callejaslaura96@gmail.com>
2021-07-23 17:54:30 +0000

    added point 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(parameters, n);
 	if (separator == NULL)
 	{
-		return;
+		separator = "";
 	}
 	for (i = 0; i < n; i++)
 	{
