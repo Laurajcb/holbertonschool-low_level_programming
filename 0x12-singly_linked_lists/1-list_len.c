d8c6f687d006a936e0d7aa9e20bfd2b3adf8b834
Laurajcb <callejaslaura96@gmail.com>
2021-07-30 19:59:08 +0000

    added point 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list-len- elements of a list_t list.
+ * @h:head of my linked list
+ *
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		counter += 1;
+	}
+return (counter);
+}
