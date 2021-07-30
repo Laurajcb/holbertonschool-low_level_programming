10fd460bbafd18d70d26fad1832c24576ad47409
Laurajcb <callejaslaura96@gmail.com>
2021-07-30 18:59:07 +0000

    Fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  * print_list- prints all the elements of a list_t list.
  * @h:head of my linked list
  *
- * Return : number of nodes
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
