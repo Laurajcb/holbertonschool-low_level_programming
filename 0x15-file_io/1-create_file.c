773ef6a79f515a63e57663662d9a7ecefd454f8d
Laurajcb <callejaslaura96@gmail.com>
2021-08-09 23:24:10 +0000

    fix Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,6 +26,11 @@ int create_file(const char *filename, char *text_content)
 			leng++;
 		}
 	}
+	else
+	{
+		text_content = "";
+	}
+	
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, leng);
 
