7c0d9869ca1dc7c5a51ee0e23260131105edab2d
Laurajcb <callejaslaura96@gmail.com>
2021-08-10 02:29:06 +0000

    fix point 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,40 +17,36 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-			exit(97);
+		    exit(97);
 	file_one = open(argv[1], O_RDONLY);
 	if (file_one == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]),
-			exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 	file_two = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_two == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-				argv[2]),
-			exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	exit(99);
 
 	r = 1024;
 	while (r == 1024)
 	{
 		r = read(file_one, buffer, 1024);
 		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-					argv[1]),
-				exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 		w = write(file_two, buffer, r);
 		if (w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-					argv[2]),
-				exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	c = close(file_one);
 	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_one),
-			exit(100);
+		    exit(100);
 	c = close(file_two);
 	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_two),
-			exit(100);
+		    exit(100);
 
 	return (0);
 }
