fd31eae0d742d6e365a46f7247ac68d2ed5ad8d2
Laurajcb <callejaslaura96@gmail.com>
2021-08-10 02:32:38 +0000

    fix point 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
 		    exit(97);
 	file_one = open(argv[1], O_RDONLY);
 	if (file_one == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 	exit(98);
 	file_two = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_two == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 	exit(99);
 
 	r = 1024;
@@ -32,11 +32,11 @@ int main(int argc, char *argv[])
 	{
 		r = read(file_one, buffer, 1024);
 		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
 		w = write(file_two, buffer, r);
 		if (w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 		exit(99);
 	}
 	c = close(file_one);
