50918dba301142d3ec0f55348920069e7149b782
Laurajcb <callejaslaura96@gmail.com>
2021-09-16 21:40:09 +0000

    fix points 2, 3

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,25 +1,31 @@
 #include "lists.h"
 /**
-* dlistint_len - retunr all the elements of a dlistint_t list
-* @h: head of the list
+* add_dnodeint - retunr all the elements of a dlistint_t list
+* @head: head of the list
+* @n: number
 * Return: number of elements
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new_node = NULL;
 
-    new_node = malloc(sizeof(dlistint_t *));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-    if (head == NULL)
-    {
-        return (NULL);
-    }
-    new_node->n = n;
-    new_node->next = *head;
+	new_node = malloc(sizeof(dlistint_t *));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    *head = new_node;
-    return (*head);
+	new_node->n = n;
+	if (*head)
+	{
+		new_node->next = *head;
+		new_node->prev = (*head)->prev;
+		(*head)->prev = new_node;
+		*head = new_node;
+		return (*head);
+	}
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+	return (*head);
 }
