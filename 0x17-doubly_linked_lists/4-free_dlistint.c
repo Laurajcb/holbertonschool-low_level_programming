6141939f8bbeab284b803e15fd2cb7f80d2b3f38
Laurajcb <callejaslaura96@gmail.com>
2021-09-16 23:37:53 +0000

    fix code

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,10 +6,10 @@
 */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *current = NULL;
+    dlistint_t *current = head;
     dlistint_t *next = NULL;
 
-    while (head != NULL)
+    while (current != NULL)
     {
         next = current->next;
         free(current);
