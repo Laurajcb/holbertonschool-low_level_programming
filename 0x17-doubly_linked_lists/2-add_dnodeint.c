ff3f715d7f340aacc6acb64ade61084ce30f1186
Laurajcb <callejaslaura96@gmail.com>
2021-09-17 18:24:00 +0000

    fixed betty and code on 7

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
-* add_dnodeint - retunr all the elements of a dlistint_t list
+* add_dnodeint-that adds a new node at the beginning of a dlistint_t list.
 * @head: head of the list
 * @n: number
-* Return: number of elements
+* Return: The address of the new element, or NULL if it failed
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
