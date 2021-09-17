9f178a4997bd98bfec518e44dadd8ba2ff901907
Laurajcb <callejaslaura96@gmail.com>
2021-09-17 13:55:32 +0000

    fixed betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,17 +2,17 @@
 /**
 * free_dlistint- frees a dlistint_t lis
 * @head: head of the list
-* Return: number of elements
+* Return: frees list of elements
 */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *current = head;
-    dlistint_t *next = NULL;
+	dlistint_t *current = head;
+	dlistint_t *next = NULL;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
 }
